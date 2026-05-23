f764a4b861497e0b842c5e686f5c03d780725908
Olamom77 <abdulmalikabdulgafar46@gmail.com>
2026-05-23 17:33:47 +0100

    Add home route and rename home_page function

--- templates/app.py
+++ templates/app.py
@@ -24,3 +24,10 @@ def contact():
 
 if __name__ == '__main__':
     app.run(debug=True)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/home')
+def home_page():  # changed name here
+    return render_template('home.html')
